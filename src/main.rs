1dfaff0d16c58026fda3f4f2b8b994a36be3a5b6
Gonza <gonzalososa342@gmail.com>
2024-05-30 22:05:24 -0300

    Adding fn to Car struct

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 #[derive(Debug)]
 struct Car {
     color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32),
 }
 
 #[derive(PartialEq, Debug)]
@@ -15,7 +15,14 @@ enum Transmission {
     Automatic,
 }
 
+#[derive(Debug)]
+enum Age {
+    New,
+    Used,
+}
+
 // Enum to create a Mileage with default value when Empty
+#[derive(Debug)]
 enum Mileage {
     Empty,
     Is(u32),
@@ -27,36 +34,104 @@ impl Default for Mileage {
     }
 }
 
+impl From<u32> for Mileage {
+    fn from(value: u32) -> Self {
+        Mileage::Is(value)
+    }
+}
+
+impl Mileage {
+    fn to_u32(mileage: &Mileage) -> u32 {
+        match mileage {
+            Mileage::Empty => 0,
+            Mileage::Is(value) => *value,
+        }
+    }
+}
+
+impl PartialEq for Mileage {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Is(l0), Self::Is(r0)) => l0 == r0,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
 // Create a car instance
-fn car_factory(
-    color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: Mileage,
-) -> Car {
+fn car_factory(color: String, motor: Transmission, roof: bool, mileage: Mileage) -> Car {
+    // Show details about car order
+    // - Check if order is for Used or New car, then check the roof type
+    // - Print details for New or Used car based on roof type
+    if mileage != Mileage::Empty && mileage != Mileage::Is(0) {
+        if roof {
+            println!(
+                "Prepare a used car: {:?}, {}, Hard top, {} miles\n",
+                motor,
+                color,
+                Mileage::to_u32(&mileage)
+            );
+        }
+    }
+
     match mileage {
         Mileage::Empty => Car {
             color,
-            transmission,
-            convertible,
-            mileage: 0,
+            motor,
+            roof,
+            age: car_quality(0),
         },
         Mileage::Is(valid_value) => Car {
             color,
-            transmission,
-            convertible,
-            mileage: valid_value,
+            motor,
+            roof,
+            age: car_quality(valid_value),
         },
     }
 }
 
+fn car_quality(miles: u32) -> (Age, u32) {
+    if miles == 0 {
+        return (Age::New, miles);
+    }
+    (Age::Used, miles)
+}
+
 fn main() {
-    let c = car_factory(
-        String::from("red"),
-        Transmission::Automatic,
-        false,
-        Mileage::Is(32),
+    /*
+    let v1 = [0; 5];
+    let v2 = vec![0; 5];
+    let mut v3: Vec<u32> = Vec::new();*/
+
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    // Declare the car type and initial values
+    let mut car: Car;
+    let mut engine: Transmission = Transmission::Manual;
+
+    car = car_factory(String::from(colors[2]), engine, true, Mileage::Empty);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
     );
 
-    println!("{:?}", c);
+    // Car order #2: Used, Semi-automatic, roof
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[3]), engine, false, Mileage::Is(100));
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[0]), engine, true, Mileage::Is(200));
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
